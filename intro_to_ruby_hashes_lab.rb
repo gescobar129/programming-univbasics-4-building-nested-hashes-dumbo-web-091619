e35f0a77ae3a6cc6d51b7c8aea4eb8ddfb2106b7
Gaida M Escobar <gescobar129@gmail.com>
2019-09-08 23:17:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  hash = { :names => { :rent_in_dollars => {
+  hash = { :railroads => { :rent_in_dollars => {
     :one_piece_owned => 25, 
     :two_pieces_owned => 50, 
     :three_pieces_owned => 100,
